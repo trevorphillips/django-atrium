0bfc3c9682e7dc0c9ef8ea9283c11c7994234b87
Trevor Phillips <trevorcoreyphillips@gmail.com>
2019-09-23 10:23:56 -0600

    finished up most of everything.

---
+++ django_atrium/atrium.py
@@ -0,0 +1,23 @@
+import atrium
+
+from .account import Account
+from .connect_widget import ConnectWidget
+from .institution import Institution
+from .member import Member
+from .transaction import Transaction
+from .user import User
+
+try:
+    from django.conf import settings
+except ImportError:
+    raise Exception('Django has to be installed to use this module.')
+
+
+class AtriumClient(Account, ConnectWidget, Institution, Member, Transaction,
+                   User):
+    def __init__(self):
+        self.client = atrium.AtriumClient(settings.MX_API_KEY,
+                                          settings.MX_CLIENT_ID,
+                                          settings.MX_ATRIUM_URL)
+
+        super().__init__(self.client)
