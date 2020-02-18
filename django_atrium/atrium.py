f1b29d4aaecb4e55e60dd59694a0708dad90bb72
Trevor Phillips <trevorcoreyphillips@gmail.com>
2020-02-18 15:48:42 -0700

    updated django settings access

--- django_atrium/atrium.py
+++ django_atrium/atrium.py
@@ -9,14 +9,23 @@ from .member import Member
 from .transaction import Transaction
 from .user import User
 
+if not settings.configured:
+    import os
+
+    ATRIUM_API_KEY = os.environ.get("ATRIUM_API_KEY")
+    ATRIUM_CLIENT_ID = os.environ.get("ATRIUM_CLIENT_ID")
+    ATRIUM_URL = os.environ.get("ATRIUM_URL")
+
+else:
+    ATRIUM_API_KEY = settings.ATRIUM_API_KEY
+    ATRIUM_CLIENT_ID = settings.ATRIUM_CLIENT_ID
+    ATRIUM_URL = settings.ATRIUM_URL
+
 
 class AtriumClient(Account, ConnectWidget, Institution, Member, Transaction, User):
     """AtriumClient class."""
 
     def __init__(self):
         """Init for AtriumClient."""
-        self.client = atrium.AtriumClient(
-            settings.ATRIUM_API_KEY, settings.ATRIUM_CLIENT_ID, settings.ATRIUM_URL
-        )
-
+        self.client = atrium.AtriumClient(ATRIUM_API_KEY, ATRIUM_CLIENT_ID, ATRIUM_URL)
         super().__init__(self.client)
