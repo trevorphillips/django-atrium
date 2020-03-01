0bbb79b2c6b378186e9424feaa7498a1bf60f7f6
Trevor Phillips <trevorcoreyphillips@gmail.com>
2020-02-29 20:43:06 -0700

    made it so atrium client reads env variables first and setting second

--- django_atrium/atrium.py
+++ django_atrium/atrium.py
@@ -1,4 +1,6 @@
 """atrium file."""
+import os
+
 import atrium
 from django.conf import settings
 
@@ -9,30 +11,25 @@ from .member import Member
 from .transaction import Transaction
 from .user import User
 
-if not settings.configured:
-    import os
+ATRIUM_API_KEY = os.environ.get("ATRIUM_API_KEY")
+ATRIUM_CLIENT_ID = os.environ.get("ATRIUM_CLIENT_ID")
+ATRIUM_URL = os.environ.get("ATRIUM_URL")
 
-    ATRIUM_API_KEY = os.environ.get("ATRIUM_API_KEY")
-    ATRIUM_CLIENT_ID = os.environ.get("ATRIUM_CLIENT_ID")
-    ATRIUM_URL = os.environ.get("ATRIUM_URL")
-
-    if ATRIUM_API_KEY is None:
-        raise Exception(
-            "ATRIUM_API_KEY was not set in Django settings or environment variables."
-        )
-    elif ATRIUM_CLIENT_ID is None:
-        raise Exception(
-            "ATRIUM_CLIENT_ID was not set in Django settings or environment variables."
-        )
-    elif ATRIUM_URL is None:
-        raise Exception(
-            "ATRIUM_URL was not set in Django settings or environment variables."
-        )
-
-else:
+if ATRIUM_API_KEY is None and settings.configured:
     ATRIUM_API_KEY = settings.ATRIUM_API_KEY
+
+if ATRIUM_CLIENT_ID is None and settings.configured:
     ATRIUM_CLIENT_ID = settings.ATRIUM_CLIENT_ID
+
+if ATRIUM_URL is None and settings.configured:
     ATRIUM_URL = settings.ATRIUM_URL
+
+if ATRIUM_API_KEY is None:
+    raise Exception("Unable to determine ATRIUM_API_KEY variable.")
+if ATRIUM_CLIENT_ID is None:
+    raise Exception("Unable to determine ATRIUM_CLIENT_ID variable.")
+if ATRIUM_URL is None:
+    raise Exception("Unable to determine ATRIUM_URL variable.")
 
 
 class AtriumClient(Account, ConnectWidget, Institution, Member, Transaction, User):
